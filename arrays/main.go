5d7da77c31deed302d2b3216ab438ecf7ae37047
Marcus Mann <mpereirassa@gmail.com>
2017-03-23 18:28:39 -0300

    Adicionando Arrays

---
+++ arrays/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var numeros [3]int
+	// numeros[0] = 1
+	// numeros[1] = 5
+	// numeros[2] = 3
+
+	numeros := [2]int{1, 2}
+
+	fmt.Println(numeros)
+}
